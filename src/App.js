708663bd18739339013c95ae61ed50b98fdd6e38
Majdou_Dev <msabri.majdouline@gmail.com>
2022-09-25 20:41:58 +0100

    add cart and favorite

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from "./shared/Navbar/navbar";
+import arabica from "./assets/images/Arabica.png"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar/>
+      <div className="flex justify-center">
+        <img src={arabica} alt="CoverImage" className="h-96 " style={{width:"90%",height:"600px"}}/>
+      </div>
+    </>
   );
 }
 
